973e4e687434fd3cb7e0085f306cfd5c46b7b2b3
rafaelaguiar10 <rafaelaguiar10@yahoo.com.br>
2023-11-24 11:10:43 -0300

    Estilizado o Menu Hamburger

--- javascript/MenuHamburger.js
+++ javascript/MenuHamburger.js
@@ -1,21 +1,25 @@
-function changeMenu() {
-    if (document.getElementById('menu__header').style.display == 'none') {
-    document.getElementById('menu__header').style.display = 'flex' 
-    document.getElementById('menu__header').style.marginLeft = '15px'
-    document.getElementById('parallax').style.marginTop = '60px'
-    document.getElementById('Title__Principal').style.marginTop = '80px'
-    document.getElementById('projetos__paragrafo').style.marginTop = '150px'
-    document.getElementById('botao__calculo').style.marginTop = '-90px'
-    document.getElementById('navbar').style.height = '130px'
-    }
-    else {
-      document.getElementById('menu__header').style.display = 'none'
-      document.getElementById('parallax').style.marginTop = '15px'
-      document.getElementById('Title__Principal').style.marginTop = '30px'
-      document.getElementById('projetos__paragrafo').style.marginTop = '96px'
-      document.getElementById('botao__calculo').style.marginTop = '-100px'
-      document.getElementById('navbar').style.height = '130px'
-
-    }
+function changeMenuMobile() {
+  if (document.getElementById('menu__header').style.display == 'none') {
+  document.getElementById('container__hamburger').style.display = 'grid'
+  document.getElementById('container__hamburger').style.marginLeft = '40px'
+  document.getElementById('parallax').style.marginBottom = '0px'
+  document.getElementById('Title__Principal').style.marginTop = '1000px'
+  document.getElementById('projetos__paragrafo').style.marginTop = '1000px'
+  document.getElementById('botao__calculo').style.marginTop = '0px'
   }
+  else {
+    document.getElementById('container__hamburger').style.display = 'none'
+    document.getElementById('parallax').style.marginTop = '15px'
+    document.getElementById('Title__Principal').style.marginTop = '30px'
+    document.getElementById('projetos__paragrafo').style.marginTop = '96px'
+    document.getElementById('botao__calculo').style.marginTop = '-100px'
+    document.getElementById('navbar').style.height = '100px'
+
+  }
+}
+
+function fecharMenu(){
+  document.getElementById('menu__header').style.display = 'none'
+}
+
   
--- javascript/MenuHamburgerTablet.js
+++ javascript/MenuHamburgerTablet.js
@@ -1,13 +1,13 @@
-function changeMenu() {
+function changeMenuTablet() {
     if (document.getElementById('menu__header').style.display == 'none') {
     document.getElementById('menu__header').style.display = 'flex' 
     document.getElementById('menu__header').style.marginTop = '60px'
     document.getElementById('menu__header').style.marginLeft = '50px'
-    document.getElementById('parallax').style.marginTop = '120px'
-    document.getElementById('Title__Principal').style.marginTop = '100px'
-    document.getElementById('projetos__paragrafo').style.marginTop = '180px'
-    document.getElementById('botao__calculo').style.marginTop = '-70px'
-    document.getElementById('navbar').style.height = '130px'
+    document.getElementById('parallax').style.marginTop = '400px'
+    document.getElementById('Title__Principal').style.marginTop = '30px'
+    document.getElementById('projetos__paragrafo').style.marginTop = '100px'
+    document.getElementById('botao__calculo').style.marginTop = '-85px'
+    document.getElementById('cloce__hamburger').style.display = 'flex'
     }
     else {
       document.getElementById('menu__header').style.display = 'none'
@@ -19,3 +19,8 @@ function changeMenu() {
 
     }
   }
+
+  function fecharMenu(){
+    document.getElementById('menu__header').style.display = 'none'
+  }
+
