3eddbc05b0789a42729df1fcc604891c2f718bf4
rafaelaguiar10 <rafaelaguiar10@yahoo.com.br>
2023-12-13 17:52:39 -0300

    Ajustado estilização do menu hamburger (mobile e tablet)

--- javascript/MenuHamburgerTablet.js
+++ javascript/MenuHamburgerTablet.js
@@ -3,10 +3,10 @@ function changeMenuTablet() {
     document.getElementById('menu__header').style.display = 'flex' 
     document.getElementById('menu__header').style.marginTop = '60px'
     document.getElementById('menu__header').style.marginLeft = '50px'
-    document.getElementById('parallax').style.marginTop = '400px'
-    document.getElementById('Title__Principal').style.marginTop = '30px'
-    document.getElementById('projetos__paragrafo').style.marginTop = '100px'
-    document.getElementById('botao__calculo').style.marginTop = '-85px'
+    document.getElementById('parallax').style.marginTop = '430px'
+    document.getElementById('Title__Principal').style.marginTop = '60px'
+    document.getElementById('projetos__paragrafo').style.marginTop = '130px'
+    document.getElementById('botao__calculo').style.marginTop = '-115px'
     document.getElementById('cloce__hamburger').style.display = 'flex'
     }
     else {
